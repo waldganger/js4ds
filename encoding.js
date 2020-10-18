f88efa709ee3e38f01107d9c007119a2baec75c8
waldganger <antony.merle@yahoo.fr>
2020-10-18 22:57:59 +0200

    "Encoding part 2 ok"

--- encoding.js
+++ encoding.js
@@ -1,4 +1,18 @@
 'use strict'
 
-const encoding = document.querySelector("meta").getAttribute("charset");
-console.log(encoding);
+// const encoding = document.querySelector("meta").getAttribute("charset");
+// console.log(encoding);
+
+function getMeta(){
+    document.querySelectorAll("meta").forEach(meta => {
+        console.log(meta.nodeName);
+        for (let i = 0; i < meta.attributes.length; i++){
+            console.log(`${meta.attributes.item(i).name} : ${meta.attributes.item(i).value}`)
+            
+        }
+        
+        
+    })
+}
+
+getMeta();
